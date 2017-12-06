951cc9fbb9c66f6ffa57e5294cef9e2d421e61f4
Ankit Mishra <anmishr@microsoft.com>
2017-12-06 13:09:20 -0800

    Adding support for publishing to blob feed

--- scripts/cibuild/PublishArtifactsFromVsts.ps1
+++ scripts/cibuild/PublishArtifactsFromVsts.ps1
@@ -10,26 +10,41 @@ myget feeds.
 
 param
 (
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
+    [switch]$PublishToMyGet,
+
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
     [string]$NuGetBuildFeedUrl,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
     [string]$NuGetBuildSymbolsFeedUrl,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
     [string]$DotnetCoreFeedUrl,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
     [string]$DotnetCoreSymbolsFeedUrl,
 
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
     [string]$NuGetBuildFeedApiKey,
 
-    [Parameter(Mandatory=$True)]
-    [string]$DotnetCoreFeedApiKey
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToMyGet')]
+    [string]$DotnetCoreFeedApiKey,
+
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToBlobFeed')]
+    [switch]$PublishToBlobFeed,
+
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToBlobFeed')]
+    [string]$NuGetBlobFeedUrl,
+
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToBlobFeed')]
+    [string]$NuGetBlobSymbolsFeedUrl,
+
+    [Parameter(Mandatory=$True, ParameterSetName='PublishToBlobFeed')]
+    [string]$NuGetBlobFeedApiKey
 )
 
-function Push-ToMyGet {
+function Push-Packages {
     [CmdletBinding(SupportsShouldProcess=$True)]
     param(
         [parameter(ValueFromPipeline=$True, Mandatory=$True, Position=0)]
@@ -38,22 +53,31 @@ function Push-ToMyGet {
         [string] $BuildFeed,
         [string] $SymbolSource
     )
-    Process {
-        $NupkgFiles | %{
-            $Feed = Switch -Wildcard ($_) {
+    Process 
+    {
+        foreach ($Nupkg in $NupkgFiles)
+        {
+            # switch case to check if the nupkg path contains symbols.nupkg
+            $Feed = Switch -Wildcard ($Nupkg) 
+            {
                 "*.symbols.nupkg" { $SymbolSource }
                 Default { $BuildFeed }
             }
-            $opts = 'push', $_, $ApiKey, '-Source', $Feed
-            if ($VerbosePreference) {
+
+            $opts = 'push', $Nupkg, $ApiKey, '-Source', $Feed
+            
+            if ($VerbosePreference) 
+            {
                 $opts += '-verbosity', 'detailed'
             }
 
-            if ($pscmdlet.ShouldProcess($_, "push to '${Feed}'")) {
+            if ($pscmdlet.ShouldProcess($Nupkg, "push to '${Feed}'")) 
+            {
                 Write-Output "$NuGetExe $opts"
                 & $NuGetExe $opts
-                if (-not $?) {
-                    Write-Error "Failed to push a package '$_' to myget feed '${Feed}'. Exit code: ${LASTEXITCODE}"
+                if (-not $?) 
+                {
+                    Write-Error "Failed to push a package '$Nupkg' to myget feed '${Feed}'. Exit code: ${LASTEXITCODE}"
                 }
             }
         }
@@ -66,9 +90,19 @@ $NuGetExe = Join-Path $env:BUILD_REPOSITORY_LOCALPATH .nuget\nuget.exe
 if(Test-Path $NupkgsDir)
 {
     Write-Output "Publishing nupkgs from '$Nupkgs'"
-    # Push all nupkgs to the nuget-build feed on myget.
-    Get-Item "$NupkgsDir\*.nupkg"  | Push-ToMyGet -ApiKey $NuGetBuildFeedApiKey -BuildFeed $NuGetBuildFeedUrl -SymbolSource $NuGetBuildSymbolsFeedUrl
-    # We push NuGet.Build.Tasks.Pack nupkg to dotnet-core feed as per the request of the CLI team.
-    Get-Item "$NupkgsDir\NuGet.Build.Tasks.Pack*.nupkg" | Push-ToMyGet -ApiKey $DotnetCoreFeedApiKey -BuildFeed $DotnetCoreFeedUrl -SymbolSource $DotnetCoreSymbolsFeedUrl
+
+    if($PublishToMyGet)
+    {
+        # Push all nupkgs to the nuget-build feed on myget.
+        Get-Item "$NupkgsDir\*.nupkg"  | Push-Packages -ApiKey $NuGetBuildFeedApiKey -BuildFeed $NuGetBuildFeedUrl -SymbolSource $NuGetBuildSymbolsFeedUrl
+        # We push NuGet.Build.Tasks.Pack nupkg to dotnet-core feed as per the request of the CLI team.
+        Get-Item "$NupkgsDir\NuGet.Build.Tasks.Pack*.nupkg" | Push-ToMyGet -ApiKey $DotnetCoreFeedApiKey -BuildFeed $DotnetCoreFeedUrl -SymbolSource $DotnetCoreSymbolsFeedUrl
+    }
+
+    if($PublishToBlobFeed)
+    {
+        # Push all nupkgs to the blob feed on azure.
+        Get-Item "$NupkgsDir\*.nupkg"  | Push-Packages -ApiKey $NuGetBlobFeedApiKey -BuildFeed $NuGetBlobFeedUrl -SymbolSource $NuGetBlobFeedUrl
+    }
 }
 
